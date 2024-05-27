5a1e60baeb44851dc7370fe823a051287272058d
e-bold <enkhmunkh@att.net>
2024-05-26 19:23:40 -0500

    Revisit

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -1,4 +1,7 @@
 ﻿namespace BalancedBracketsTests;
+using BalancedBrackets;
+
+
 
 [TestClass]
 public class BalancedBracketsTests
@@ -6,8 +9,37 @@ public class BalancedBracketsTests
     // TODO: Add tests to this file.
 
     [TestMethod]
-    public void EmptyTest()
+    public void OnlyBracketsReturnsTrue()
     {
-        Assert.AreEqual(true, true);
+        string testData = "[]";
+        bool actualResult = BalancedBrackets.HasBalancedBrackets(testData);
+        string message = "A single pair of balanced brackets returns true";
+        Assert.IsTrue(actualResult, message);
     }
+
+    [TestMethod]
+    public void DoublePairOfBracketsReturnsTrue(){
+        string testData2 = "[[]]";
+        bool actualResult2 = BalancedBrackets.HasBalancedBrackets(testData2);
+        string message2 = "A double pair of balanced brackets returns true";
+        Assert.IsTrue(actualResult2, message2);
+    }
+
+    [TestMethod]
+    public void SingleBracketReturnsFalse(){
+        string testData3 = "[";
+        bool actualResult3 = BalancedBrackets.HasBalancedBrackets(testData3);
+        string message3 = "A single bracket returns false";
+        Assert.IsFalse(actualResult3, message3);
+    }
+
+    [TestMethod]
+    public void InvertedBracketsReturnsFalse(){
+        string testData4 = "][";
+        bool actualResult4 = BalancedBrackets.HasBalancedBrackets(testData4);
+        string message4 = "A pair of inverted brackets returns false";
+        Assert.IsTrue(actualResult4, message4);
+    }
+    
+
 }
